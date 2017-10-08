3fa17eb8ee0a5f19f8626fb2cdca031550dd3bad
BastiaanOlij <mux213@gmail.com>
2017-10-08 20:32:24 +1100

    All functions are now complete, just need to add some sauce to commit_eye and we can start porting

--- demo/Main.gd
+++ demo/Main.gd
@@ -3,16 +3,36 @@ extends Spatial
 var arvr_interface = null
 
 func _ready():
-	var test_interface = ARVRInterfaceGDNative.new()
-	test_interface.set_gdnative_library(preload("res://arvrsimple.tres"))
+	# lets go gdnative!
+	arvr_interface = ARVRInterfaceGDNative.new()
+	arvr_interface.set_gdnative_library(preload("res://arvrsimple.tres"))
 	
-	print("Test " + test_interface.get_name())
+	# testing
+	print("Test " + arvr_interface.get_name())
+	print("Test " + String(arvr_interface.get_recommended_render_targetsize()))
 	
 #	arvr_interface = preload("res://ARVRSimple.gdns").new()
 #	arvr_interface = ARVRServer.find_interface("ARVRSimple")
-	arvr_interface = ARVRServer.get_interface(ARVRServer.get_interface_count()-1)
+#	arvr_interface = ARVRServer.get_interface(ARVRServer.get_interface_count()-1)
 	if arvr_interface and arvr_interface.initialize():
 		get_viewport().arvr = true
-		
-		#this needs to move into the interface itself
-		#ARVRServer.set_primary_interface(arvr_interface)
+
+func _process(delta):
+	# Test for escape to close application, space to reset our reference frame
+	if (Input.is_key_pressed(KEY_ESCAPE)):
+		get_tree().quit()
+	elif (Input.is_key_pressed(KEY_SPACE)):
+		# Calling center_on_hmd will cause the ARVRServer to adjust all tracking data so the player is centered on the origin point looking forward
+		ARVRServer.center_on_hmd(true, true)
+
+	# We minipulate our origin point to move around. Note that with roomscale tracking a little more then this is needed
+	# because we'll rotate around our origin point, not around our player. But that is a subject for another day.
+	if (Input.is_key_pressed(KEY_LEFT)):
+		$ARVROrigin.rotation.y += delta
+	elif (Input.is_key_pressed(KEY_RIGHT)):
+		$ARVROrigin.rotation.y -= delta
+
+	if (Input.is_key_pressed(KEY_UP)):
+		$ARVROrigin.translation -= $ARVROrigin.transform.basis.z * delta;
+	elif (Input.is_key_pressed(KEY_DOWN)):
+		$ARVROrigin.translation += $ARVROrigin.transform.basis.z * delta;
--- src/ARVRSimple.c
+++ src/ARVRSimple.c
@@ -14,6 +14,11 @@
 
 typedef struct arvr_data_struct {
 	bool is_initialised;
+	float iod_cm;
+	float display_to_lens_cm;
+	float display_width_cm;
+	float oversample;
+	float eye_height;
 } arvr_data_struct;
 
 const godot_gdnative_api_struct *api = NULL;
@@ -32,20 +37,25 @@ void GDN_EXPORT godot_nativescript_init(void *p_handle) {
 
 }
 
-void *arvr_constructor(void *p_object) {
+void *arvr_constructor(godot_object *p_instance) {
 	godot_string ret;
 
 	printf("ARVRSimple.arvr_constructor()\n");
 
 	arvr_data_struct *arvr_data = api->godot_alloc(sizeof(arvr_data_struct));
 	arvr_data->is_initialised = false;
+	arvr_data->iod_cm = 6.1;
+	arvr_data->display_to_lens_cm = 4.0;
+	arvr_data->display_width_cm = 14.0;
+	arvr_data->oversample = 1.0;
+	arvr_data->eye_height = 1.80;
 
 	printf("Allocated data at %p\n", arvr_data);
 
 	return arvr_data;
 }
 
-void arvr_destructor(void *p_object, void *p_data) {
+void arvr_destructor(godot_object *p_instance, void *p_data) {
 	printf("ARVRSimple.arvr_destructor()\n");
 
 	if (p_data != NULL) {
@@ -55,17 +65,17 @@ void arvr_destructor(void *p_object, void *p_data) {
 	}
 }
 
-godot_string arvr_get_name(void *p_object, void *p_data) {
+godot_string arvr_get_name(godot_object *p_instance, void *p_data) {
 	godot_string ret;
 
-	printf("ARVRSimple.arvr_get_name(%p, %p)\n", p_object, p_data);
+	printf("ARVRSimple.arvr_get_name(%p, %p)\n", p_instance, p_data);
 	char name[] = "ARVRSimple";
 	api->godot_string_new_data(&ret, name, strlen(name));
 
 	return ret;
 }
 
-godot_int arvr_get_capabilities(void *p_object, void *p_data) {
+godot_int arvr_get_capabilities(godot_object *p_instance, void *p_data) {
 	godot_int ret;
 
 	printf("ARVRSimple.arvr_get_capabilities()\n");
@@ -74,7 +84,7 @@ godot_int arvr_get_capabilities(void *p_object, void *p_data) {
 	return ret;
 };
 
-godot_bool arvr_get_anchor_detection_is_enabled(void *p_object, void *p_data) {
+godot_bool arvr_get_anchor_detection_is_enabled(godot_object *p_instance, void *p_data) {
 	godot_bool ret;
 
 	printf("ARVRSimple.arvr_get_anchor_detection_is_enabled()\n");
@@ -83,13 +93,13 @@ godot_bool arvr_get_anchor_detection_is_enabled(void *p_object, void *p_data) {
 	return ret;
 };
 
-void arvr_set_anchor_detection_is_enabled(void *p_object, void *p_data, bool p_enable) {
+void arvr_set_anchor_detection_is_enabled(godot_object *p_instance, void *p_data, bool p_enable) {
 	printf("ARVRSimple.arvr_set_anchor_detection_is_enabled()\n");
 
 	// we ignore this, not supported in this interface!
 };
 
-godot_bool arvr_is_stereo(void *p_object, void *p_data) {
+godot_bool arvr_is_stereo(godot_object *p_instance, void *p_data) {
 	godot_bool ret;
 
 	printf("ARVRSimple.arvr_is_stereo()\n");
@@ -98,7 +108,7 @@ godot_bool arvr_is_stereo(void *p_object, void *p_data) {
 	return ret;
 };
 
-godot_bool arvr_is_initialized(void *p_object, void *p_data) {
+godot_bool arvr_is_initialized(godot_object *p_instance, void *p_data) {
 	godot_bool ret;
 	arvr_data_struct *arvr_data = (arvr_data_struct *)p_data;
 
@@ -108,231 +118,165 @@ godot_bool arvr_is_initialized(void *p_object, void *p_data) {
 	return ret;
 };
 
-/*
+godot_bool arvr_initialize(godot_object *p_instance, void *p_data) {
+	godot_bool ret;
+	arvr_data_struct * arvr_data = (arvr_data_struct *) p_data;
 
-godot_variant ARVRSimple_is_initialized(godot_object *p_instance, void *p_method_data, void *p_user_data, int p_num_args, godot_variant **p_args) {
-	godot_variant ret;
-	user_data_struct * user_data = (user_data_struct *) p_user_data;
+	printf("ARVRSimple.arvr_initialize()\n");
 
-	printf("ARVRSIMPLE.is_initialized()\n");
-	api->godot_variant_new_bool(&ret, user_data->is_initialised);
+	if (!arvr_data->is_initialised) {
+		// initialise this interface, so initialize any 3rd party libraries, open up HMD window if required, etc.
 
-	return ret;
-};
 
-godot_variant ARVRSimple_initialize(godot_object *p_instance, void *p_method_data, void *p_user_data, int p_num_args, godot_variant **p_args) {
-	godot_variant ret;
-	user_data_struct * user_data = (user_data_struct *) p_user_data;
-
-	printf("ARVRSIMPLE.initialize()\n");
-
-	if (!user_data->is_initialised) {
-		// initialise this interface
-
-		///@TODO how to do this in GDNative?
-		// ARVRServer *arvr_server = ARVRServer::get_singleton();
-		// arvr_server->set_primary_interface(this);
-
-		user_data->is_initialised = true;
+		// note, this will be made the primary interface by ARVRInterfaceGDNative
+		arvr_data->is_initialised = true;
 	};
 
 	// and return our result
-	api->godot_variant_new_bool(&ret, user_data->is_initialised);
+	ret = arvr_data->is_initialised;
 	return ret;
 };
 
-godot_variant ARVRSimple_uninitialize(godot_object *p_instance, void *p_method_data, void *p_user_data, int p_num_args, godot_variant **p_args) {
-	godot_variant ret;
-	user_data_struct * user_data = (user_data_struct *) p_user_data;
+void arvr_uninitialize(godot_object *p_instance, void *p_data) {
+	arvr_data_struct * arvr_data = (arvr_data_struct *) p_data;
 
-	printf("ARVRSIMPLE.uninitialize()\n");
+	printf("ARVRSimple.arvr_uninitialize()\n");
 
-	if (user_data->is_initialised) {
+	if (arvr_data->is_initialised) {
+		// note, this will already be removed as the primary interface by ARVRInterfaceGDNative
+
 		// cleanup if needed
 
-		user_data->is_initialised = false;
+		arvr_data->is_initialised = false;
 	};
-
-	// This shouldn't need a return variable but not sure how to set that up yet so just return true
-	api->godot_variant_new_bool(&ret, true);
-	return ret;
 };
 
-godot_variant ARVRSimple_get_recommended_render_targetsize(godot_object *p_instance, void *p_method_data, void *p_user_data, int p_num_args, godot_variant **p_args) {
-	godot_variant ret;
+godot_vector2 arvr_get_recommended_render_targetsize(godot_object *p_instance, void *p_data) {
 	godot_vector2 size;
 
-	printf("ARVRSIMPLE.get_recommended_render_targetsize()\n");
+	printf("ARVRSimple.arvr_get_recommended_render_targetsize()\n");
 
 	api->godot_vector2_new(&size, 500, 500);
-	api->godot_variant_new_vector2(&ret, &size);
+
+	return size;
+};
+
+godot_transform arvr_get_transform_for_eye(godot_object *p_instance, void *p_data, godot_int p_eye, godot_transform *p_cam_transform) {
+	arvr_data_struct * arvr_data = (arvr_data_struct *) p_data;
+	godot_transform transform_for_eye;
+	godot_transform hmd_transform;
+	godot_transform reference_frame;
+	godot_transform ret;
+	godot_vector3 offset;
+	godot_real world_scale = 1.0; ///@TODO call arvr_server->get_world_scale()
+
+	printf("ARVRSimple.arvr_get_transform_for_eye()\n");
+
+	// create our transform from head center to eye
+	api->godot_transform_new_identity(&transform_for_eye);
+	if (p_eye == 1) {
+		// left eye
+		api->godot_vector3_new(&offset, -arvr_data->iod_cm * 0.01 * 0.5 * world_scale, 0.0, 0.0);
+		transform_for_eye = api->godot_transform_translated(&transform_for_eye, &offset);
+	} else if (p_eye == 2) {
+		// right eye
+		api->godot_vector3_new(&offset, arvr_data->iod_cm * 0.01 * 0.5 * world_scale, 0.0, 0.0);
+		transform_for_eye = api->godot_transform_translated(&transform_for_eye, &offset);
+	} else {
+		// leave in the middle, mono
+	}
+
+	// now determine our HMD positional tracking, we have none in this interface...
+	api->godot_transform_new_identity(&hmd_transform);
+	api->godot_vector3_new(&offset, 0.0, arvr_data->eye_height * world_scale, 0.0);
+	hmd_transform = api->godot_transform_translated(&hmd_transform, &offset);
+
+	// and apply our reference frame, we need to call arvr_server->get_reference_frame()
+	api->godot_transform_new_identity(&reference_frame);
+
+	// Now construct our full transform, the order may be in reverse, have to test :)
+	ret = *p_cam_transform;
+	ret = api->godot_transform_operator_multiply(&ret, &reference_frame);
+	ret = api->godot_transform_operator_multiply(&ret, &hmd_transform);
+	ret = api->godot_transform_operator_multiply(&ret, &transform_for_eye);
 
 	return ret;
 };
 
-godot_variant ARVRSimple_get_transform_for_eye(godot_object *p_instance, void *p_method_data, void *p_user_data, int p_num_args, godot_variant **p_args) {
-	godot_variant ret;
-	godot_transform transform;
+void arvr_set_frustum(godot_real *p_projection, godot_real p_left, godot_real p_right, godot_real p_bottom, godot_real p_top, godot_real p_near, godot_real p_far) {
 
-	printf("ARVRSIMPLE.get_transform_for_eye()\n");
+	godot_real x = 2 * p_near / (p_right - p_left);
+	godot_real y = 2 * p_near / (p_top - p_bottom);
 
-	api->godot_transform_new_identity(&transform);
-	api->godot_variant_new_transform(&ret, &transform);
+	godot_real a = (p_right + p_left) / (p_right - p_left);
+	godot_real b = (p_top + p_bottom) / (p_top - p_bottom);
+	godot_real c = -(p_far + p_near) / (p_far - p_near);
+	godot_real d = -2 * p_far * p_near / (p_far - p_near);
 
-	return ret;
+	p_projection[0] = x;
+	p_projection[1] = 0;
+	p_projection[2] = 0;
+	p_projection[3] = 0;
+	p_projection[4] = 0;
+	p_projection[5] = y;
+	p_projection[6] = 0;
+	p_projection[7] = 0;
+	p_projection[8] = a;
+	p_projection[9] = b;
+	p_projection[10] = c;
+	p_projection[11] = -1;
+	p_projection[12] = 0;
+	p_projection[13] = 0;
+	p_projection[14] = d;
+	p_projection[15] = 0;
 };
 
-godot_variant ARVRSimple_get_projection_for_eye(godot_object *p_instance, void *p_method_data, void *p_user_data, int p_num_args, godot_variant **p_args) {
-	godot_variant ret;
-	godot_pool_real_array projection;
+void arvr_return_projection_for_eye(godot_object *p_instance, void *p_data, godot_real *p_projection, godot_int p_eye, godot_real p_aspect, godot_real p_z_near, godot_real p_z_far) {
+	arvr_data_struct * arvr_data = (arvr_data_struct *) p_data;
 
-	printf("ARVRSIMPLE._get_projection_for_eye()\n");
-	api->godot_variant_new_pool_real_array(&ret, &projection);
+	// The code below is an example of calculating our stereoscopic projections based on head mounted devices.
 
-	return ret;
+	printf("ARVRSimple.arvr_return_projection_for_eye()\n");
+
+	// we first calculate our base frustum on our values without taking our lens magnification into account.
+	godot_real f1 = (arvr_data->iod_cm * 0.5) / arvr_data->display_to_lens_cm;
+	godot_real f2 = ((arvr_data->display_width_cm - arvr_data->iod_cm) * 0.5) / arvr_data->display_to_lens_cm;
+	godot_real f3 = (arvr_data->display_width_cm / 4.0) / arvr_data->display_to_lens_cm;
+
+	// now we apply our oversample factor to increase our FOV. how much we oversample is always a balance we strike between performance and how much
+	// we're willing to sacrifice in FOV.
+	godot_real add = ((f1 + f2) * (arvr_data->oversample - 1.0)) / 2.0;
+	f1 += add;
+	f2 += add;
+
+	// always apply KEEP_WIDTH aspect ratio
+	f3 *= p_aspect;
+
+	if (p_eye == 1) {
+		// left eye
+		arvr_set_frustum(p_projection, -f2 * p_z_near, f1 * p_z_near, -f3 * p_z_near, f3 * p_z_near, p_z_near, p_z_far);
+	} else {
+		// right eye
+		arvr_set_frustum(p_projection, -f1 * p_z_near, f2 * p_z_near, -f3 * p_z_near, f3 * p_z_near, p_z_near, p_z_far);		
+	}
 };
 
-godot_variant ARVRSimple_commit_for_eye(godot_object *p_instance, void *p_method_data, void *p_user_data, int p_num_args, godot_variant **p_args) {
-	godot_variant ret;
+void arvr_commit_for_eye(godot_object *p_instance, void *p_data, godot_int p_eye, godot_rid p_rid, godot_rect2 *p_screen_rect) {
+	// This function is responsible for outputting the final render buffer for each eye. 
+	// p_screen_rect will only have a value when we're outputting to the main viewport.
 
-	printf("ARVRSIMPLE.commit_for_eye()\n");
+	// For an interface that must output to the main viewport (such as with mobile VR) we should give an error when p_screen_rect is not set
+	// For an interface that outputs to an external device we should render a copy of one of the eyes to the main viewport if p_screen_rect is set, and only output to the external device if not.
 
-	// This shouldn't need a return variable but not sure how to set that up yet so just return true
-	api->godot_variant_new_bool(&ret, true);
-	return ret;
+	printf("ARVRSimple.arvr_commit_for_eye()\n");
+
+
+
 };
 
-godot_variant ARVRSimple_process(godot_object *p_instance, void *p_method_data, void *p_user_data, int p_num_args, godot_variant **p_args) {
-	godot_variant ret;
+void arvr_process(godot_object *p_instance, void *p_data) {
+	// this method gets called before every frame is rendered, here is where you should update tracking data, update controllers, etc.
 
-	printf("ARVRSIMPLE.process()\n");
+	printf("ARVRSimple.arvr_process()\n");
 
-	// This shouldn't need a return variable but not sure how to set that up yet so just return true
-	api->godot_variant_new_bool(&ret, true);
-	return ret;
 };
-
-void GDN_EXPORT godot_nativescript_init(void *p_handle) {
-	{
-		godot_instance_create_func create = {};
-		create.create_func = &ARVRSimple_constructor;
-
-		godot_instance_destroy_func destroy = {};
-		destroy.destroy_func = &ARVRSimple_destructor;
-
-		api->godot_nativescript_register_class(p_handle, "ARVRSimple", "ARVRScriptInterface", create, destroy);		
-	}
-
-	{
-		godot_instance_method get_data = {};
-		get_data.method = &ARVRSimple_get_name;
-
-		godot_method_attributes attributes = {};
-		attributes.rpc_type = GODOT_METHOD_RPC_MODE_DISABLED;
-
-		api->godot_nativescript_register_method(p_handle, "ARVRSimple", "get_name", attributes, get_data);
-	}
-
-	{
-		godot_instance_method get_data = {};
-		get_data.method = &ARVRSimple_get_capabilities;
-
-		godot_method_attributes attributes = {};
-		attributes.rpc_type = GODOT_METHOD_RPC_MODE_DISABLED;
-
-		api->godot_nativescript_register_method(p_handle, "ARVRSimple", "capabilities", attributes, get_data);
-	}
-
-	{
-		godot_instance_method get_data = {};
-		get_data.method = &ARVRSimple_is_stereo;
-
-		godot_method_attributes attributes = {};
-		attributes.rpc_type = GODOT_METHOD_RPC_MODE_DISABLED;
-
-		api->godot_nativescript_register_method(p_handle, "ARVRSimple", "is_stereo", attributes, get_data);
-	}
-
-	{
-		godot_instance_method get_data = {};
-		get_data.method = &ARVRSimple_is_initialized;
-
-		godot_method_attributes attributes = {};
-		attributes.rpc_type = GODOT_METHOD_RPC_MODE_DISABLED;
-
-		api->godot_nativescript_register_method(p_handle, "ARVRSimple", "is_initialized", attributes, get_data);
-	}
-
-	{
-		godot_instance_method get_data = {};
-		get_data.method = &ARVRSimple_initialize;
-
-		godot_method_attributes attributes = {};
-		attributes.rpc_type = GODOT_METHOD_RPC_MODE_DISABLED;
-
-		api->godot_nativescript_register_method(p_handle, "ARVRSimple", "initialize", attributes, get_data);
-	}
-
-	{
-		godot_instance_method get_data = {};
-		get_data.method = &ARVRSimple_uninitialize;
-
-		godot_method_attributes attributes = {};
-		attributes.rpc_type = GODOT_METHOD_RPC_MODE_DISABLED;
-
-		api->godot_nativescript_register_method(p_handle, "ARVRSimple", "uninitialize", attributes, get_data);
-	}
-
-	{
-		godot_instance_method get_data = {};
-		get_data.method = &ARVRSimple_get_recommended_render_targetsize;
-
-		godot_method_attributes attributes = {};
-		attributes.rpc_type = GODOT_METHOD_RPC_MODE_DISABLED;
-
-		api->godot_nativescript_register_method(p_handle, "ARVRSimple", "get_recommended_render_targetsize", attributes, get_data);
-	}
-
-	{
-		godot_instance_method get_data = {};
-		get_data.method = &ARVRSimple_get_transform_for_eye;
-
-		godot_method_attributes attributes = {};
-		attributes.rpc_type = GODOT_METHOD_RPC_MODE_DISABLED;
-
-		api->godot_nativescript_register_method(p_handle, "ARVRSimple", "get_transform_for_eye", attributes, get_data);
-	}
-
-	{
-		godot_instance_method get_data = {};
-		get_data.method = &ARVRSimple_get_projection_for_eye;
-
-		godot_method_attributes attributes = {};
-		attributes.rpc_type = GODOT_METHOD_RPC_MODE_DISABLED;
-
-		api->godot_nativescript_register_method(p_handle, "ARVRSimple", "_get_projection_for_eye", attributes, get_data);
-	}
-
-	{
-		godot_instance_method get_data = {};
-		get_data.method = &ARVRSimple_commit_for_eye;
-
-		godot_method_attributes attributes = {};
-		attributes.rpc_type = GODOT_METHOD_RPC_MODE_DISABLED;
-
-		api->godot_nativescript_register_method(p_handle, "ARVRSimple", "commit_for_eye", attributes, get_data);
-	}
-
-	{
-		godot_instance_method get_data = {};
-		get_data.method = &ARVRSimple_process;
-
-		godot_method_attributes attributes = {};
-		attributes.rpc_type = GODOT_METHOD_RPC_MODE_DISABLED;
-
-		api->godot_nativescript_register_method(p_handle, "ARVRSimple", "process", attributes, get_data);
-	}
-
-	///@TODO should make an instance of this interface and register it with our ARVRServer, need to find out how :)
-}
-*/
